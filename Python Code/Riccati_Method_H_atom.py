0f7c138e687ce50925d8cc15e37675b54b89fdf8
Pmorande27 <pmorande27@gmail.com>
2023-03-03 18:03:45 +0000

    Add quartic oscillator values

--- Python Code/Riccati_Method_H_atom.py
+++ Python Code/Riccati_Method_H_atom.py
@@ -4,11 +4,15 @@ import matplotlib.pyplot as plt
 x = Symbol('x',positive = True)
 ebar = Symbol('ebar',positive = True)
 r = Symbol('r',positive = True)
+Abar = Symbol('Abar')
+e = Symbol('e',positive = True)
+V = Abar* r**4
+#V = -ebar**2/r
 k = Symbol('k', positive = True)
-e = Symbol('e',positive=True)
-V = -ebar**2/r
+A = Symbol('A',positive=True)
 Veff = 1/(8* r**2) + V
-r0 = 1/(4* ebar**2)
+r0 = 4**(1/(-5))* (Abar *4)**(1/(-6))
+#r0 = 1/(4*ebar**2)
 #print(Veff)
 Veffbar = factor(simplify(Veff.subs(r,x+r0)- Veff.subs(r,r0)))
 #print(Veffbar)
@@ -16,9 +20,12 @@ Veffbar = factor(simplify(Veff.subs(r,x+r0)- Veff.subs(r,r0)))
 
 #Getting the first coefficients
 Eminustwo = Veff.subs(r,r0)
+#print(Eminustwo)
 phiminusone =  simplify(-sqrt(2* Veffbar))
+#print(phiminusone)
 Eminusone = -1/2* diff(phiminusone,x).subs(x,0) - 1/(2* r0**2)
-phizero = factor(simplify((1/2 * diff(phiminusone,x) + 1/(2*(x+r0)**2) + Eminusone)/(-phiminusone)))
+#print(Eminusone)
+phizero = simplify((1/2 * diff(phiminusone,x) + 1/(2*(x+r0)**2) + Eminusone)/(-phiminusone))
 Ezero = -1/2 *diff(phizero,x).subs(x,0) - 1/2* phizero.subs(x,0)**2 + 3/(8* r0**2)
 phione = simplify((1/2* diff(phizero,x) + 1/2* phizero**2 - 3/(8* (x+r0)**2) + Ezero)/(-phiminusone))
 Eone = -1/2* diff(phione,x).subs(x,0) - 1/2 *(phione.subs(x,0)* phizero.subs(x,0) + phione.subs(x,0)* phizero.subs(x,0))
@@ -61,10 +68,13 @@ def approximate_to_order(Elistinit,phiszero,order):
     else:
         length = len(Elist)
     Energy_list = [klist[i]*Elist[i] for i in range(length)]
-    Energy_value = sum([Energy_list[i].subs([(ebar,e/k),(k,3)]) for i in range(len(Energy_list))])
-    Real_value =- e**4/2
+    Energy_value = sum([Energy_list[i].subs([(Abar,1/k),(k,3)]) for i in range(len(Energy_list))])
+    #Energy_value = sum([Energy_list[i].subs([(ebar,e/k),(k,3)]) for i in range(len(Energy_list))])
+    #Real_value = -e**4/2
+    Real_value =2.393644
     error = abs((Energy_value-Real_value)/Real_value)
     accuracy = 1-error
+    #print(accuracy)
     return (Energy_list,error)
 
 #print(approximate_to_order(Elist,phiszero=phis0,order= 5))
@@ -72,8 +82,8 @@ def plot(n):
     n_list = [i for i in range(1,n+1)]
     error_list = [0]*n
     for j in range(n):
+        #print(j+1)
         error_list[j] = approximate_to_order(Elistinit=Elist,phiszero=phis0,order=n_list[j])[1]
-    plt.plot(n_list,error_list)
-    plt.show()
+    print(n_list,error_list)
 
-plot(50)
+plot(10)
